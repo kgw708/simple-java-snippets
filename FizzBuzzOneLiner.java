b56b1b06c9134b1645b809945ca595fef36950fd
kgw708 <kigawanaoya@gmail.com>
2023-01-06 22:36:48 +0900

    add some snippets

---
+++ FizzBuzzOneLiner.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+public class FizzBuzzOneLiner {
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 100)
+            .mapToObj(i -> (i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i))
+            .forEach(System.out::println);
+    }
+}
