c2775a4b5878a0399862b2614f5361dcc980ba99
kgw708 <kigawanaoya@gmail.com>
2023-01-13 21:15:56 +0900

    add Map2StringDemo.java

---
+++ Map2StringDemo.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Map2StringDemo {
+    public static void main(String[] args) {
+        Map<String, Long> map = new HashMap<>();
+        map.put("hoge", Long.valueOf(123));
+        map.put("fuga", Long.valueOf(456));
+        map.put("piyo", Long.valueOf(789));
+
+        String str = map.entrySet().stream()
+            .map(e -> e.getKey() + ":" + e.getValue())
+            .collect(Collectors.joining(", "));
+        System.out.println(str);
+    }
+}
