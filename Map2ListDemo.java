35b457e2cb782344ab188a0705ea8afa563ee988
kgw708 <kigawanaoya@gmail.com>
2023-01-14 19:05:53 +0900

    add Map2ListDemo.java

---
+++ Map2ListDemo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Map2ListDemo {
+    public static void main(String[] args) {
+        Map<String, Long> map = new HashMap<>();
+        map.put("hoge", Long.valueOf(123));
+        map.put("fuga", Long.valueOf(456));
+        map.put("piyo", Long.valueOf(789));
+
+        List<String> keyList = new ArrayList<>(map.keySet());
+        System.out.println(keyList.toString());
+
+        List<Long> valueList = new ArrayList<>(map.values());
+        System.out.println(valueList.toString());
+    }
+}
