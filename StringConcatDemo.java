4aed3b86c183b2da555b94ca54026eda1591c6d1
kgw708 <kigawanaoya@gmail.com>
2023-01-17 23:05:55 +0900

    add snippets

---
+++ StringConcatDemo.java
@@ -0,0 +1,13 @@
+public class StringConcatDemo {
+    public static void main(String[] args) {
+        String hoge = "hoge";
+        String fuga = "fuga";
+        String piyo = "piyo";
+        System.out.println("+演算子による文字列連結: " + hoge + fuga + piyo);
+        System.out.println("concatによる文字列連結: " + hoge.concat(fuga).concat(piyo));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(hoge).append(fuga).append(piyo);
+        System.out.println("StringBuilderによる文字列連結: " + sb.toString());
+    }
+}
