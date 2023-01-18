500744d8239b7ac52490a0cbc7643734565daeb3
kgw708 <kigawanaoya@gmail.com>
2023-01-18 21:24:54 +0900

    add StringConversion.java

---
+++ StringConversion.java
@@ -0,0 +1,9 @@
+public class StringConversion {
+    public static void main(String[] args) {
+        String lower = "abc";
+        System.out.println("lower to upper: " + lower.toUpperCase());
+
+        String upper = "ABC";
+        System.out.println("upper to lower: " + upper.toLowerCase());
+    }
+}
