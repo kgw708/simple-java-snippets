1a3fcef95de8d022711087dcc424adf36c0ca39e
kgw708 <kigawanaoya@gmail.com>
2023-01-22 23:47:14 +0900

    add some snippets

---
+++ snippets/RegexDemo.java
@@ -0,0 +1,26 @@
+package snippets;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo {
+    public static void main(String[] args) {
+
+        // 電話番号
+        String phoneNumber = "0120-1234-5678";
+        Pattern p1 = Pattern.compile("\\d{2,4}-\\d{2,4}-\\d{4}");
+        Matcher m1 = p1.matcher(phoneNumber);
+        System.out.println(m1.find());
+
+        // 郵便番号
+        String postalNumber = "100-0000";
+        Pattern p2 = Pattern.compile("^\\d{3}-\\d{4}$");
+        Matcher m2 = p2.matcher(postalNumber);
+        System.out.println(m2.find());
+
+        // etc
+        String text = "abcd1234";
+        System.out.println(text.matches("[a-z0-9]+"));
+        System.out.println(text.matches("[a-z]+"));
+    }
+}
--- snippets/SystemEnvCheck.java
+++ snippets/SystemEnvCheck.java
@@ -1,15 +1,19 @@
 package snippets;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SystemEnvCheck {
     public static void main(String[] args) {
         Map<String, String> map = System.getenv();
 
         for (Map.Entry<String, String> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + "=" + entry.getValue());
+            System.out.println(entry.getKey() + "='" + entry.getValue() + "'");
         }
 
-        // System.out.println(System.getProperties());
+        String props = System.getProperties().entrySet().stream()
+            .map(e -> e.getKey() + "='" + e.getValue() + "'")
+            .collect(Collectors.joining("\n"));
+        System.out.println(props);
     }
 }
