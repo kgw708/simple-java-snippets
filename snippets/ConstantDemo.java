b7bb09660ea9c6121b9e4832cbc605d6e1ff898e
kgw708 <kigawanaoya@gmail.com>
2023-01-24 23:23:59 +0900

    update ConstantDemo.java

--- snippets/ConstantDemo.java
+++ snippets/ConstantDemo.java
@@ -1,12 +1,24 @@
 package snippets;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ConstantDemo {
 
     private static final String HOGE = "hoge";
+
+    private static final List<String> BOOKS_LIST;
+    static {
+        List<String> list = new ArrayList<>();
+        list.add("magazine");
+        list.add("comic");
+        list.add("novel");
+        list.add("dictionary");
+        BOOKS_LIST = Collections.unmodifiableList(list);
+    }
 
     private static final Map<String, Integer> FRUITS_MAP;
     static {
@@ -22,6 +34,7 @@ public class ConstantDemo {
 
     public static void main(String[] args) {
         System.out.println(HOGE);
+        System.out.println(BOOKS_LIST);
         System.out.println(FRUITS_MAP);
     }
 }
