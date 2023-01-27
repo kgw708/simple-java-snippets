e6ac3575988532d2fee984289965d8395bf9a5a0
kgw708 <kigawanaoya@gmail.com>
2023-01-27 21:53:19 +0900

    add NullPointerExceptionDemo.java

---
+++ snippets/NullPointerExceptionDemo.java
@@ -0,0 +1,12 @@
+package snippets;
+
+public class NullPointerExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
