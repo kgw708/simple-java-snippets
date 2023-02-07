41bbc01c8e535d429ff32c0e47519b41517dd513
kgw708 <kigawanaoya@gmail.com>
2023-02-07 23:03:12 +0900

    add some snippets

---
+++ snippets/method/PrivateMethod1.java
@@ -0,0 +1,21 @@
+package snippets.method;
+
+public class PrivateMethod1 {
+    public static void main(String[] args) {
+        sayGoodMorning();
+        sayGoodAfternoon();
+        sayGoodEvening();
+    }
+
+    private static void sayGoodMorning() {
+        System.out.println("Good morning!");
+    }
+
+    private static void sayGoodAfternoon() {
+        System.out.println("Good afternoon!");
+    }
+
+    private static void sayGoodEvening() {
+        System.out.println("Good evening!");
+    }
+}
---
+++ snippets/method/PrivateMethod2.java
@@ -0,0 +1,12 @@
+package snippets.method;
+
+public class PrivateMethod2 {
+    public static void main(String[] args) {
+        int result = add(20, 30);
+        System.out.println(result);
+    }
+    
+    private static int add(int a, int b) {
+        return a + b;
+    }
+}
