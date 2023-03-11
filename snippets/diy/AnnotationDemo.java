1f6a8c7dacd80fc85bdf56655c363f9880c1940a
kgw708 <kigawanaoya@gmail.com>
2023-03-11 22:12:52 +0900

    add some snippets

---
+++ snippets/diy/AnnotationDemo.java
@@ -0,0 +1,21 @@
+package snippets.diy;
+
+// 自作したアノテーションを付与するためのクラス
+// クラスにアノテーション付与するパターン
+@MyAnnotation(name="class", value=1)
+public class AnnotationDemo {
+
+    // フィールドにアノテーションを付与するパターン
+    @MyAnnotation(name="field", value=2)
+    private String name;
+
+    // コンストラクタにアノテーションを付与するパターン
+    @MyAnnotation(name="constructor", value=3)
+    public AnnotationDemo() {}
+
+    // メソッドにアノテーションを付与するパターン
+    @MyAnnotation(name="method", value=4)
+    public void execute() {
+        System.out.println("execute");
+    }
+}
---
+++ snippets/diy/AnnotationExample.java
@@ -0,0 +1,31 @@
+package snippets.diy;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+// 自作したアノテーションを付与したクラスをリフレクションにより動作確認する
+public class AnnotationExample {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, NoSuchMethodException, SecurityException {
+
+        // クラスに付与したアノテーションの取得
+        Class<?> clazz = Class.forName("snippets.diy.AnnotationDemo");
+        MyAnnotation myClass = (MyAnnotation) clazz.getAnnotation(MyAnnotation.class);
+        System.out.println("Class : name = " + myClass.name() + ", value = " + myClass.value());
+
+        // フィールドに付与したアノテーションの取得
+        Field field = clazz.getDeclaredField("name");
+        MyAnnotation myField = (MyAnnotation) field.getAnnotation(MyAnnotation.class);
+        System.out.println("Field : name = " + myField.name() + ", value = " + myField.value());
+
+        // コンストラクタに付与したアノテーションの取得
+        Constructor<?> constructor = clazz.getConstructor();
+        MyAnnotation myConstructor = (MyAnnotation) constructor.getAnnotation(MyAnnotation.class);
+        System.out.println("Constructor : name = " + myConstructor.name() + ", value = " + myConstructor.value());
+
+        // メソッドに付与したアノテーションの取得
+        Method method = clazz.getMethod("execute");
+        MyAnnotation myMethod = (MyAnnotation) method.getAnnotation(MyAnnotation.class);
+        System.out.println("Method : name = " + myMethod.name() + ", value = " + myMethod.value());
+    }
+}
